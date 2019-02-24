d81af5ed0d01c745d6acc698a4b6e89f863d32d6
Michael Černý <michalcern25@gmail.com>
2019-02-24 17:49:00 +0100

    export původní projekt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <React.Fragment>
+        <header className="header">
+          <nav className="nav main-nav container">
+            <ul>
+              <li>
+                <a className="nav-home" href="main.html">
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a className="nav-login" href="login.html">
+                  LOG IN
+                </a>
+              </li>
+            </ul>
+          </nav>
         </header>
-      </div>
+        <main>
+          <section className="container">
+            <p className="main-section-text">
+              <img
+                className="main-section-image"
+                src="images\db00eab2acc2c526d94f0aebb4aa9e0c.png"
+              />
+              Spaco: la fina limo. Ĉi tiuj estas la vojaĝoj de la stratŝipo
+              Enterprise. Lia kvinjara misio: esplori strangajn novajn mondojn.
+              Serĉi novan vivon kaj novajn civilizaciojn. Aŭdacxe iri, kie neniu
+              antaŭeniris!
+            </p>
+          </section>
+        </main>
+        <footer className="main-footer">
+          <p>
+            <small>Copyright 2019 by Michael Černý</small>
+          </p>
+        </footer>
+      </React.Fragment>
     );
   }
 }
